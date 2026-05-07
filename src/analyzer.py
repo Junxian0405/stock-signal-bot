71fa103050d6f36bff963ddfe83992d3ec121d73
Junxian0405 <124035007+Junxian0405@users.noreply.github.com>
2026-05-08 00:49:03 +0800

    Update analyzer.py

--- src/analyzer.py
+++ src/analyzer.py
@@ -18,7 +18,7 @@ TELEGRAM_TOKEN   = os.environ["TELEGRAM_TOKEN"]
 TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 WATCHLIST = [
-    "MU", "SNDK", "NVDA", "TSLA"
+    "MU", "SNDK"
 ]
 
 ET = pytz.timezone("America/New_York")
